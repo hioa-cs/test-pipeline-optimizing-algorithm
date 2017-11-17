f5d70edfd92eeed4afe6a15e5c2ec28de33e8afc
Taiyeba <staiyeba@gmail.com>
2017-11-17 23:49:13 +0100

    fixing scripts for case 3

--- case3_computations_multiplesets.sh
+++ case3_computations_multiplesets.sh
@@ -3,17 +3,17 @@
 for (( n=1; n<=10; n++ ))
 do
   echo "Running computations on Test $n"
-  python computations.py datasets/100tests_max_80_8/t$n.csv datasets/100tests_max_80_8/t$n.csv_sorted.csv
+  python test_computations.py datasets/case_3/100tests_max_80_8/t$n.csv datasets/case_3/100tests_max_80_8/t$n.csv_sorted.csv
 done
 
 for (( n=1; n<=10; n++ ))
 do
   echo "Running computations on Test $n"
-  python computations.py datasets/100tests_mid_50_3/t$n.csv datasets/100tests_mid_50_3/t$n.csv_sorted.csv
+  python test_computations.py datasets/case_3/100tests_mid_50_3/t$n.csv datasets/case_3/100tests_mid_50_3/t$n.csv_sorted.csv
 done
 
 for (( n=1; n<=10; n++ ))
 do
   echo "Running computations on Test $n"
-  python computations.py datasets/100tests_min_20_2/t$n.csv datasets/100tests_min_20_2/t$n.csv_sorted.csv
+  python test_computations.py datasets/case_3/100tests_min_20_2/t$n.csv datasets/case_3/100tests_min_20_2/t$n.csv_sorted.csv
 done
--- case3_reorder_multiplesets.sh
+++ case3_reorder_multiplesets.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 
-for tf in datasets/100tests_max_80_8/*.csv; do
+for tf in datasets/case_3/100tests_max_80_8/*.csv; do
   python test_reordering_algorithm_fast.py "$tf"
 done
 
-for tf in datasets/100tests_mid_50_3/*.csv; do
+for tf in datasets/case_3/100tests_mid_50_3/*.csv; do
   python test_reordering_algorithm_fast.py "$tf"
 done
 
-for tf in datasets/100tests_min_20_2/*.csv; do
+for tf in datasets/case_3/100tests_min_20_2/*.csv; do
   python test_reordering_algorithm_fast.py "$tf"
 done
