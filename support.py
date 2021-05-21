c4d1fe27c78117a90d32cad7c44a669d619532c8
MadeleineKongshavn <Madekoro@hotmail.com>
2021-05-21 15:18:31 +0200

    Changed function get_dataset()

--- support.py
+++ support.py
@@ -1,8 +1,21 @@
 import csv
+import ast
 
+def convert_dictionary(element):
+    element['Id'] = int(element['Id'])
+    element['Duration'] = int(element['Duration'])
+    element['CalcPrio'] = int(element['CalcPrio'])
+    element['LastResults'] = ast.literal_eval( element['LastResults'] )
+    element['Verdict'] = int(element['Verdict'])
+    element['Cycle'] = int(element['Cycle'])
+    element['Depend'] = []
 
 def get_dataset(file_name):
+    dataset = []
     with open(file_name, newline="\n") as csvfile:
         reader = csv.DictReader(csvfile,  delimiter=';')
         for row in reader:
-            print(row)
+            convert_dictionary(row)
+            dataset.append(row)
+    return dataset
+
